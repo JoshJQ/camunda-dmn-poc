4f0f521c679ff71e4b42bb74275aaf9d1ab3e8c4
Josh Jiang <qi.jiang@ingka.com>
2021-02-22 15:28:34 -0400

    Add integration test

--- src/main/java/com/db/camunda/response/BasicResponseMessage.java
+++ src/main/java/com/db/camunda/response/BasicResponseMessage.java
@@ -2,9 +2,11 @@ package com.db.camunda.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class BasicResponseMessage {
     private String message;
 }
